8ad7be5cd88cac0d08db813e582e93b439e58785
JosephMumo <josephmumo25@gmail.com>
2023-07-22 19:28:33 +0300

    update

--- application/myapp/src/components/Hero.jsx
+++ application/myapp/src/components/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
             <h1 className='text-3xl font-bold'>We Provide Effective <br /> Legal Solutions</h1>
             <br />
             <div className='p-2'>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia magni voluptatum molestiae distinctio placeat odio id, odit commodi ducimus voluptas repellendus voluptates reiciendis corporis asperiores accusantium minus maxime animi omnis necessitatibus in. Illo rerum sunt non facilis sint nam aliquid, minima ut eius accusantium excepturi doloremque mollitia soluta nemo veniam!</p>
+                <p>Lorem, ipsum dolor amet consectetur adipisicing elit. Quia magni voluptatum molestiae distinctio placeat odio id, odit commodi ducimus voluptas repellendus voluptates reiciendis corporis asperiores accusantium minus maxime animi omnis necessitatibus in. Illo rerum sunt non facilis sint nam aliquid, minima ut eius accusantium excepturi doloremque mollitia soluta nemo veniam!</p>
             </div>
             <br />
             <Form/>
